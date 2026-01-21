58e79ac84fb5cc24d17e1d1fff7463aee134c4b9
Irene Fernandez Rebollo <irene.fernandez@fht.org>
2026-01-21 14:35:11 +0100

    Added min/max/median age to supplementary table 1

--- src/supplementary_table_1.R
+++ src/supplementary_table_1.R
@@ -18,6 +18,10 @@ tcga_samples_projects <- merge(tcga_samples, projects, by.x = "project", by.y = 
 # Get Supplementary Table 1
 df <- tcga_samples_projects %>%
   group_by(project, name) %>%  # Group by project
-  summarise(number_samples = n(), .groups = "drop")  # Count rows
-colnames(df) <- c("Project ID", "Project name", "Number of samples")
+  summarise(number_samples = n(),
+            min_age = min(age_at_index),
+            max_age = max(age_at_index),
+            median_age = median(age_at_index),
+            .groups = "drop")  # Count rows
+colnames(df) <- c("Project ID", "Project name", "Number of samples", "Min Age", "Max Age", "Median Age")
 writexl::write_xlsx(df, "Supplementary Tables/Supplementary_Table_1.xlsx")
