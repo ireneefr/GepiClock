97f5113010f972c476fbdaf41f2ba6e89fe942ad
Irene Fernandez Rebollo <irene.fernandez@fht.org>
2026-02-16 15:41:17 +0100

    Change supplementary figures 15 colors according to figure 1

--- src/supplementary_figure_15.R
+++ src/supplementary_figure_15.R
@@ -1,3 +1,7 @@
+###################################
+###   Supplementary Figure 15   ###
+###################################
+
 setwd("/Volumes/iorio/Irene/epiclock_dev")
 source("src/utils.R")
 dir_results <- "results/TCGA/"
@@ -41,46 +45,17 @@ clocks_performance <- clocks_performance[clocks_performance$Clock != "RebolloI20
 
 # Plot
 color_type <- c("Primary Tumour (Test)" = "steelblue1",
-                "Primary Tumour (Others)" = "steelblue3",
+                "Primary Tumour (Others)" = "blueviolet",
                 "Normal Tissue" = "chartreuse3")
-# png(filename = "/group/iorio/Irene/epiclock/plots/missing_coefficients.png",
-#     width = 12, height = 7, units = 'in', res = 600)
-ggplot(res, aes(x = Clock_name, y = mean, fill = data, color = data)) +
-  geom_col(position = position_dodge(width = 0.8), width = 0.8) +
-  geom_errorbar(aes(ymin = mean - sd, ymax = mean + sd),
-                width = 0.02, position = position_dodge(width = 0.8), color = "black") +
-  scale_fill_manual(values = color_type, name = "Data subset") +
-  scale_color_manual(values = color_type, name = "Data subset") +
-  theme_bw(base_size = 20) +
-  xlab("Missing Coefficients (%)") + ylab("Pearson Correlation (R)") 
-# dev.off()
-
-png(filename = "/Volumes/iorio/Irene/epiclock/plots/rebollovsclocks_performance.png",
-    width = 12, height = 7, units = 'in', res = 600)
-ggplot(clocks_performance, aes(x = Clock, y = R, fill = Sample_type, color = Sample_type)) +
-  geom_col(position = position_dodge(width = 0.8), width = 0.8) +
-  scale_fill_manual(values = color_type, name = "Data subset") +
-  scale_color_manual(values = color_type, name = "Data subset") +
-  
-  geom_errorbar(data = res, aes(x = Clock_name, y = mean, ymin = mean - sd, ymax = mean + sd, group = data),
-                width = 0.1, position = position_dodge(width = 0.8), color = "black", inherit.aes = FALSE) +
-  geom_point(data = res, aes(x = Clock_name, y = mean, group = data, shape = "Mean ± SD"), size = 3,
-             position = position_dodge(width = 0.8), color = "red", inherit.aes = FALSE) +
-  scale_shape_manual(name = "Random CpGs subset", values = 16) +
-  
-  scale_x_discrete(limits=rev) +
-  theme_bw(base_size = 20) +
-  xlab("") + ylab("Pearson Correlation (R)") +
-  coord_flip()
-dev.off()
 
 pdf("/Volumes/iorio/Irene/epiclock_dev/Supplementary Figures/Supplementary_Figure_15.pdf",
     width = 12, height = 7)
-ggplot(clocks_performance, aes(x = Clock, y = R, fill = Sample_type, color = Sample_type)) +
+ggplot(clocks_performance, aes(x = Clock, y = R, fill = Sample_type)) +
   geom_col(position = position_dodge(width = 0.8), width = 0.8) +
-  scale_fill_manual(values = color_type, name = "Data subset") +
-  scale_color_manual(values = color_type, name = "Data subset") +
-  
+  scale_fill_manual(values = color_type, name = "Data subset",
+                    labels = c("Normal samples\n(Independent set)", "Tumour samples\n(Independent set)", "Tumour samples\n(Test set)")) +
+  guides(fill = guide_legend(reverse = TRUE)) +
+
   geom_errorbar(data = res, aes(x = Clock_name, y = mean, ymin = mean - sd, ymax = mean + sd, group = data),
                 width = 0.1, position = position_dodge(width = 0.8), color = "black", inherit.aes = FALSE) +
   geom_point(data = res, aes(x = Clock_name, y = mean, group = data, shape = "Mean ± SD"), size = 3,
@@ -89,6 +64,7 @@ ggplot(clocks_performance, aes(x = Clock, y = R, fill = Sample_type, color = Sam
   
   scale_x_discrete(limits=rev) +
   theme_bw(base_size = 20) +
+  theme(axis.text = element_text(color = "black")) +
   xlab("") + ylab("Pearson Correlation (R)") +
   coord_flip()
 dev.off()
